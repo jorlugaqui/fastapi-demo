8cd9237277f62adbe5f823cf73a16ffbd29c256e
Jorge Galvis <jgalvis@H9467.ad.hugeinc.com>
2020-09-23 08:06:53 -0500

    Initial App.

---
+++ main.py
@@ -0,0 +1,38 @@
+import logging
+import random
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+log = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+log.addHandler(handler)
+
+
+STUDENTS_DB = [
+    'Fernando Dev',
+    'Omar Dev',
+    'Arley Dev',
+]
+
+
+@app.get('/api/v1/developers/')
+async def get_developers():
+    return {'developers': STUDENTS_DB}
+
+
+@app.get('/api/v1/developers/random')
+async def get_developers_random():
+    return {
+        'developer': random.choice(STUDENTS_DB)
+    }
+
+
+@app.get('/api/v1/developers/{index}')
+async def get_developer_by_index(index: int):
+    try:
+        return {'developer': STUDENTS_DB[index]}
+    except IndexError:
+        log.warning('Defaulting to index 0: first developer in the list')
+        return {'developer': STUDENTS_DB[0]}
